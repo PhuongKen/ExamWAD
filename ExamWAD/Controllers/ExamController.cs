0dd6dc9cc087710ae10ccf62f174e292e19989a8
phuongken <you@example.com>
2018-12-04 10:09:27 +0700

    nop bai thi thuc hanh

---
+++ ExamWAD/Controllers/ExamController.cs
@@ -0,0 +1,45 @@
+﻿using ExamWAD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExamWAD.Controllers
+{
+    public class ExamController : Controller
+    {
+        ExamWADEntities db = new ExamWADEntities();
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View(db.Exams.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Exam exam)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    exam.Status = 1;
+                    db.Exams.Add(exam);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(exam);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ExamWAD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamWAD.MvcApplication" Language="C#" %>
